4b9557fa0da994771c3343b56fde2a356e54dc25
squarefozilov@gmail <squarefozilov@gmail.com>
2019-09-26 21:25:49 -0400

    1

--- assets/javascript/word-g.js
+++ assets/javascript/word-g.js
@@ -1,100 +1,106 @@
+$(document).ready(function() {
 
-console.log('for loop version word guess')
+    var possibleWords = ["florence", "paris", "madrid", "rome","new york", 
+                            "shanghai", "london", "tokyo", "sydney", "toronto", "beijing", "moscow",
+                            "hong kong", "chicago", "seoul", "los angeles"]
 
+    const maxGuess = 10
+    var pauseGame = false
 
-//var possibleWords = ["humor", "miniature", "amusing", "creepy", "fact", "risk", "verse", "land", "lumpy", "holiday", "glorious", "weigh", "brake", "pretty", "grin", "capricious", "bite-sized", "misty", "ignore", "certain", "sloppy", "dress", "true", "zonked", "observation", "action", "various", "want", "direful", "suck", "dress", "scarecrow", "judge", "madly", "quizzical", "consist", "fierce", "love", "arrest", "serve", "fit", "hug", "tan", "curve", "eatable", "tub", "race", "innocent", "open", "preach", "steady", "acoustics", "lock", "field", "arrange", "rifle", "learned", "toe", "flow", "competition", "ill-fated", "oatmeal", "match", "male", "measure", "loaf", "smile", "wrestle", "dull", "food", "locket", "bell", "beg", "strengthen", "responsible", "enchanting", "loutish", "switch", "idea", "nine", "squeamish", "pig", "bat", "dear", "trains", "owe", "frogs", "assorted", "lonely", "hurry", "natural", "sun", "snow", "obnoxious", "broken", "friend", "bright", "cake", "sour", "permit", "economic", "lovely", "quick", "van", "tempt", "apparel", "decay", "business", "adjustment", "blushing", "makeshift", "slippery", "load", "winter", "exist", "tongue", "country", "roll", "fast", "moor", "possess", "pat", "pass", "books", "impartial", "hospitable", "dust", "naughty", "extra-large", "tacky", "produce", "committee", "fuzzy", "judicious", "nebulous", "stick", "ear", "copy", "friendly", "press", "distinct", "vegetable", "upset", "venomous", "statement", "sulky", "spell", "x-ray", "square", "taste", "great", "thumb", "adjoining", "chilly", "test", "ancient", "green", "badge", "work", "repeat", "free", "elderly", "doctor", "difficult", "grubby", "approval", "turn", "vivacious", "thundering", "cherries", "rest", "plan", "crime", "sticks", "wealthy", "phone", "suspend", "gullible", "fence", "note", "wall", "interest", "coil", "jump", "enchanted", "funny", "racial", "greasy", "polish", "elbow", "smart", "bore", "crowd", "glistening", "oval", "eggs", "nauseating", "detailed", "veil", "coal"]
- 
-var possibleWords = ["florence", "paris", "madrid", "rome", "singapore", "dubai", "new york city", 
-"shanghai", "london", "tokyo", "sydney", "toronto", "beijing", "moscow",
-"johannesburg", "istanbul", "warsaw", "jakarta", "kuala lumpur", "mexico city",
-"hong kong", "chicago", "seoul", "los angeles", "mumbai"]
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
 
-var answer = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
-console.log(answer)
+    resetGame()
 
-var mylengtharr = answer.length;
-var wins=0 ;
-var display =[mylengtharr];
-var win =mylengtharr;
-var letters = answer.split('');
-var attemptsleft = 9;
-var output="";
-var userLetter="";
-var guessingWord = [];
-var guessedLetters =[];
-var losses = 0;
+    // Wait for key press
+    document.onkeypress = function(event) {
+        // Make sure key pressed is an alpha character
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
 
- setup = function(){
+    // Game Functions
+    // Check if letter is in word & process
+    function checkForLetter(letter) {
+        var foundLetter = false
+        
 
-    for(var i=0; i<answer.length;i++){
+        // Search string for letter
+        for (var i=0, j= wordToMatch.length; i<j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+               
+                // If guessing word matches random word
+                if (guessingWord.join("") === wordToMatch) {
+                    // Increment # of wins
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame,5000)
+                }
+            }
+        }
 
-        display[i]=" _ ";
-        output=output+display[i];
+        if (!foundLetter) {
+           
+            // Check if inccorrect guess is already on the list
+            if (!guessedLetters.includes(letter)) {
+                // Add incorrect letter to guessed letter list
+                guessedLetters.push(letter)
+                // Decrement the number of remaining guesses
+                numGuess--
+            }
+            if (numGuess === 0) {
+                // Display word before reseting game
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 1)
+            }
+        }
+
+        updateDisplay()
 
     }
-        console.log("-"+ output);
-        document.getElementById("p2").innerHTML = output;
-        output=" ";
+    // Check in keypressed is between A-Z or a-z
+    function isAlpha (ch){
+        return /^[A-Z]$/i.test(ch);
+    }
 
-}
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
 
-document.onkeyup = function(event) {
-         var userGuess = event.key;
-    
-         checkUserletter(userGuess);
+        // Get a new word
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
 
-         console.log(answer);
-         console.log(userGuess.toUpperCase());
-            
-        //guessedLetters.push(userGuess);
-     
-        
-       
-   }
-    
+        // Reset word arrays
+        guessedLetters = []
+        guessingWord = []
 
-   // declaring function for for checking user input
-
-   function checkUserletter(userGuess){
-        //  
-    for (var i=0, j= answer.length; i<j; i++) {
-        if (userGuess.toUpperCase() == answer[i]) {
-            guessingWord[i] = userGuess.toUpperCase();
-            i++;
-             
-            console.log('inside')
-           
-            if (guessingWord.join("") === answer) 
-            {
-
-                console.log("You won--");
-                wins++
-                i++
-                
-               
-              
+        // Reset the guessed word
+        for (var i=0, j=wordToMatch.length; i < j; i++){
+            // Put a space instead of an underscore between multi word "words"
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
             }
-            console.log(guessingWord);
         }
-        if (userGuess.toUpperCase() != answer[i]){
-                    
-        }
-          
-        }
-    
-        document.getElementById("p3").innerText = guessingWord.join("__")
-        document.getElementById("wins").innerHTML = wins;       
-        document.getElementById("p1").innerHTML = losses; 
 
+        // Update the Display
+        updateDisplay()
+    }
 
-
-
-                                        
-                                        }
-
-
-   window.onload = function(){
-         
-    setup();
-
-   }
+    function updateDisplay () {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ")
+    }
+})
